c7d54b0b1a791d639831a9466b5400bbf74f741f
isof-erik <erik.af.geijerstam@isof.se>
2026-05-19 10:30:30 +0200

    gdal-translation script

---
+++ batch_script/gdal-translate.bat
@@ -0,0 +1,7 @@
+chcp 65001 >nul
+gdal_translate -of PNG "1_Hammarkinds härad_modified_clipped2.tif" 1_Hammarkinds.png
+gdal_translate -of PNG "2_Gryts socken_modified_clipped2.tif" 2_Gryts.png
+gdal_translate -of PNG "4_Skärkinds härad_modified_clipped2.tif" 4_Skärkinds.png
+gdal_translate -of PNG "5_Skärkinds härad 1_modified_clipped2.tif" 5_Skärkinds_1.png
+gdal_translate -of PNG "7_Sankt Annes kapell 1_modified_clipped2.tif" 7_Sankt_Annes_1.png
+gdal_translate -of PNG "9_Sankt Annes kapell 3_modified_clipped2.tif" 9_Sankt_Annes_3.png
